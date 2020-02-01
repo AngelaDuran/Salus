c41d0382393ca985d20a0bc0376f34e221914168
Angela.Duran <41122156+angelahub2@users.noreply.github.com>
2020-01-31 23:42:01 -0800

    Map Landmark

--- app/src/main/java/com/example/salus/MainActivity.java
+++ app/src/main/java/com/example/salus/MainActivity.java
@@ -1,52 +1,51 @@
 package com.example.salus;
 
 import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.FragmentActivity;
+
+
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-       FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        LatLng LosAngeles = new LatLng(34.057906, -118.238845);
+        mMap.addMarker(new MarkerOptions().position(LosAngeles).title("LA Hub"));
+        LatLng SanFran = new LatLng(37.773359, -122.425250);
+        mMap.addMarker(new MarkerOptions().position(SanFran).title("SanFran Hub"));
+        LatLng SLO = new LatLng(35.283407, -120.661805);
+        mMap.addMarker(new MarkerOptions().position(SLO).title("SLO Hub"));
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+     //   mMap.moveCamera(CameraUpdateFactory.newLatLng(LosAngeles));
     }
 }
+
+
